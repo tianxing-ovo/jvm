fbcbdcc660257ee235ed36beea0af7b4c41c07d2
ltx <2050365570@qq.com>
2024-03-28 12:50:23 +0800

    串池延迟加载和编译期优化

---
+++ src/test/StringLazyLoad.java
@@ -0,0 +1,18 @@
+package test;
+
+/**
+ * 字符串延迟加载
+ */
+public class StringLazyLoad {
+    public static void main(String[] args) {
+        System.out.println();
+        // 字符串数量: 2195
+        System.out.print("1");
+        System.out.print("2");
+        // 字符串数量: 2197
+        System.out.print("1");
+        System.out.print("2");
+        // 字符串数量: 2197
+        System.out.println();
+    }
+}
