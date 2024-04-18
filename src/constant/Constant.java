9f52605185209b0679861a8857ba3683a7900c88
litianxing <2050365570@qq.com>
2024-04-18 20:28:30 +0800

    垃圾回收分析

--- src/constant/Constant.java
+++ src/constant/Constant.java
@@ -5,7 +5,9 @@ package constant;
  */
 public interface Constant {
 
+    int _512KB = 1024 * 512;
     int _4MB = 1024 * 1024 * 4;
+    int _7MB = 1024 * 1024 * 7;
     int _100MB = 1024 * 1024 * 100;
     int _1GB = 1024 * 1024 * 1024;
 
